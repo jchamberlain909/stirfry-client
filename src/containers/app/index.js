c7d88e8f704db8218966edd88f442607125beb26
jchamberlain909 <jchamberlain909@gmail.com>
2018-10-21 22:15:39 -0400

    setup initial routing

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Welcome from "../../components/welcome";
+import { Switch, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Switch>
+          <Route exact path="/" render={(routerProps)=><Welcome {...routerProps}/>}/>
+
+        </Switch>
       </div>
     );
   }
