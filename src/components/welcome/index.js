466536e9d749fc71391edc82e5e09599b622c117
jchamberlain909 <jchamberlain909@gmail.com>
2018-10-23 16:53:53 -0400

    basic welcome page

--- src/components/welcome/index.js
+++ src/components/welcome/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import './welcome.css'
+import LocalDining from "@material-ui/icons/LocalDining";
+
+
 
 const Welcome = () => {
     return ( 
-    <div>
+    <div id="welcome">
+        <h1 id="title">StirFry.me</h1>
+        <img src="/robot.png" alt="robot"/> 
+        <Button variant="contained" color="primary" size="large">
+            Get Cooking!
+            <LocalDining className="fork-knife"></LocalDining>
 
+        </Button>
     </div> );
 }
  
