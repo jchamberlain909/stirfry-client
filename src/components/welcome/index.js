73eb595352350a0ce8507a08162664706c073c02
jchamberlain909 <jchamberlain909@gmail.com>
2018-10-23 17:03:28 -0400

    added react-router link from welcome to recipe which will generate the recipe

--- src/components/welcome/index.js
+++ src/components/welcome/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import './welcome.css'
 import LocalDining from "@material-ui/icons/LocalDining";
+import { Link } from 'react-router-dom'
 
+const MyLink = props => <Link to="/recipe" {...props} />
 
 
 const Welcome = () => {
@@ -10,7 +12,7 @@ const Welcome = () => {
     <div id="welcome">
         <h1 id="title">StirFry.me</h1>
         <img src="/robot.png" alt="robot"/> 
-        <Button variant="contained" color="primary" size="large">
+        <Button variant="contained" color="primary" size="large" component={MyLink}>
             Get Cooking!
             <LocalDining className="fork-knife"></LocalDining>
 
